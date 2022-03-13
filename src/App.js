7ad8f886dbf21a1d126a97315c801fbc3c0c807b
taxil123 <64030805+taxil123@users.noreply.github.com>
2022-03-13 21:58:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/Navbar';
+import {Route,Routes} from 'react-router';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import Pricing from './components/Pricing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/blog" element={<Blog/>} />
+          <Route path="/pricing" element={<Pricing/>} />
+        </Routes>
     </div>
   );
 }
